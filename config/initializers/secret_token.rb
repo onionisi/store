832653ab59c323eedfc8d158ee8b1f351ac67da3
Chong Yang <iamyangchong@gmail.com>
2013-11-01 14:06:41 +0800

    store use rails 4.0.0 and source change to taobao

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store::Application.config.secret_key_base = 'f6fab7d9ae364ec8820e6af074642324bb7bf3de1bb7748716bf28f25d366eb51ebe3edc11cc8328196cf3af9aef0cbe3643a994b42687a5d89873e622de486e'
